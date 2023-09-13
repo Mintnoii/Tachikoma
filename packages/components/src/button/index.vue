0c59b9870f8a3be21d6749c609a31916508e852e
Mintnoii <MintnoiiQing@gmail.com>
2023-09-13 17:12:36 +0800

    wip: vite build

--- packages/components/src/button/index.vue
+++ packages/components/src/button/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <button>MY Button</button>
+  <button class="my-button" :class="buttonStyle"><slot /></button>
 </template>
 
 <script lang="ts" setup>
-defineExpose({
-  name: 'MyButton',
-})
+import { computed } from "vue";
+defineOptions({ name: "my-button" });
+type ButtonProps = {
+  type?: string;
+};
+const buttonProps = defineProps<ButtonProps>();
+
+const buttonStyle = computed(() => {
+  return { [`my-button--${buttonProps.type}`]: buttonProps.type };
+});
 </script>
+
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,29 +1,56 @@
 import { defineConfig } from "vite";
 import Vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
+import DefineOptions from "unplugin-vue-define-options/vite"
 
-// https://vitejs.dev/config/
-const rollupOptions = {
-  external: ["vue", "vue-router"],
-  output: {
-    globals: {
-      vue: "Vue",
-    },
-  },
-};
 export default defineConfig({
 
-  plugins: [Vue(),vueJsx()],
+  plugins: [Vue(),vueJsx(),
+    dts({
+      entryRoot: "./src",
+      outDir: ["../comps/es/src", "../comps/lib/src"],
+      //指定使用的 tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+      tsconfigPath: "./tsconfig.json",
+    }),
+    DefineOptions(),
+  ],
    // 添加库模式配置
   build: {
-    rollupOptions,
-    minify:false,
+    //压缩
+    minify: false,
+    rollupOptions: {
+      //忽略打包vue文件
+      external: ["vue"],
+      input: ["index.ts"],
+      output: [
+          {
+          //打包格式
+          format: "es",
+          //打包后文件名
+          entryFileNames: "[name].mjs",
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: "named",
+          //配置打包根目录
+          dir: "../comps/es",
+        },
+        {
+          //打包格式
+          format: "cjs",
+          //打包后文件名
+          entryFileNames: "[name].js",
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: "named",
+          //配置打包根目录
+          dir: "../comps/lib",
+        },
+      ]
+    },
     lib: {
-      entry: "./src/index.ts",
-      name: "ToolkitComps",
-      fileName: "toolkit-comps",
-      // 导出模块格式
-      formats: ["es", "umd","iife"],
+      entry: "./index.ts",
+      name: "comps",
     },
   },
 });
